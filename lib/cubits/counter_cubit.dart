d2bab7bbcaf55150c4734013ebf9a6d85f131ab1
Ahmad Nidal <a7mad.delea@gmail.com>
2025-05-16 06:55:13 +0300

    Add bloc package, Create Counter states & Counter cubit.

---
+++ lib/cubits/counter_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'counter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterAIncrementState());
+}
